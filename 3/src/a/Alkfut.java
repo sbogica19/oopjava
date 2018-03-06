f02758367fee250abe72e34b4ee9bd6c83c49ed7
simon26 <simon26@iit415.iit.uni-miskolc.hu>
2018-03-06 17:40:33 +0100

    4. gyakos projectek

--- 3/src/a/Alkfut.java
+++ 3/src/a/Alkfut.java
@@ -3,9 +3,39 @@ package a;
 public class Alkfut {
 
 	public static void main(String[] args) {
-
+		Alkalmazott[] alkok = new Alkalmazott[5];
+		
+		alkok[0] = new Alkalmazott();
+		alkok[0].setName("Tomi1");
+		alkok[0].setPayment(10);
+		
+		alkok[1] = new Alkalmazott();
+		alkok[1].setName("Tomi2");
+		alkok[1].setPayment(43);
+		
+		alkok[2] = new Alkalmazott();
+		alkok[2].setName("Tomi3");
+		alkok[2].setPayment(29);
+		
+		alkok[3] = new Alkalmazott();
+		alkok[3].setName("Tomi4");
+		alkok[3].setPayment(30);
+		
+		alkok[4] = new Alkalmazott();
+		alkok[4].setName("Tomi5");
+		alkok[4].setPayment(67);
+		
+		for(Alkalmazott a : alkok)
+			System.out.println(a.szovegVissza());
+		
+		int max = alkok[0].getPayment();
+		for (int i=0;i<alkok.length;i++){
+			if(alkok[i].getPayment() > max)
+				max=alkok[i].getPayment();
+		}
+		System.out.println("A legnagyobb fizetésû alkalmazott: " + max);
+		
 		Alkalmazott alkalmazott1 = new Alkalmazott();
-
 		alkalmazott1.setName("Tomi");
 		alkalmazott1.setPayment(8);
 
@@ -32,6 +62,12 @@ public class Alkfut {
 		} else{
 			System.out.println("Az alk2 fizetése a nagyobb.");
 		}
+		
+		int db=0;
+		for (int i=0;i<alkok.length;i++){
+			if(alkok[i].fizHatar(20, 50)) db++;
+		}
+		System.out.println(db + " darab alk fizetése esik az intervallumba.");
 	}
 
 }
